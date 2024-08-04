71a2aa9f23254208b5a454b62c94998055dca139
bakseck <seck.bakar@ugb.edu.sn>
2024-08-04 05:29:20 +0000

    add images

--- src/components/CatalogueSection.jsx
+++ src/components/CatalogueSection.jsx
@@ -7,8 +7,8 @@ import {
      CardFooter,
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
-// Les données des livres
 const books = [
      {
           "title": "La décharge",
@@ -18,7 +18,8 @@ const books = [
           "description": "Ce recueil peut, par excellence, être conçu comme le viscère de son auteur qui tente par-dessus tout d'exhiber ce que son cœur garde depuis bien des années lumières. Par l'entremise des mots chargés de secrets, l'auteur parle du mystère de l'univers dans son sens le plus engloutant. Le lecteur est invité à faire un voyage dans l'espace des lettres pour essayer de comprendre un peu ce qui se cache derrière les écrits, les non-dits. Ainsi ébauche-t-il par la voie par décliner par la mort; c'est le résumé succinct de l'homme à vrai dire. Le titre, LA DÉCHARGE, n'est qu'un subterfuge, mais également une échappatoire par laquelle passent les détails de l'existence humaine. Pour tout dire, le recueil traite de l'amour, des mœurs sociales, de la spiritualité, de l'importance de la lecture, pour ne citer que ceux-là.",
           "isbn": "978-2-493692-00-9",
           "price": "10€",
-          "author_info": "Daouda Dione est un natif de Kaolack du Sénégal, qui depuis sa tendre enfance, est tombé sous le charme de la belle et douce poésie. Le grand poète, Amadou Lamine Sall considère comme l'une de ces nouvelles voix africaines sur lesquelles il faut indubitablement compter pour l'avenir du monde. Ainsi, considère-t-il par l'intermédiaire de sa plume, un cri et un engagement de l'homme en général et le noir en particulier. En plus de sa qualité d'écrivain, il est enseignant dans le cycle élémentaire."
+          "author_info": "Daouda Dione est un natif de Kaolack du Sénégal, qui depuis sa tendre enfance, est tombé sous le charme de la belle et douce poésie. Le grand poète, Amadou Lamine Sall considère comme l'une de ces nouvelles voix africaines sur lesquelles il faut indubitablement compter pour l'avenir du monde. Ainsi, considère-t-il par l'intermédiaire de sa plume, un cri et un engagement de l'homme en général et le noir en particulier. En plus de sa qualité d'écrivain, il est enseignant dans le cycle élémentaire.",
+          "image": "/la-decharge.jpg"
      },
      {
           "title": "Dans la peau d'une femme",
@@ -27,7 +28,8 @@ const books = [
           "description": "Dans les toilettes, je m'adonne tel un bébé dans un bassin rempli. L'eau coule à travers mon corps, elle ne peut pas s'arrêter, ma peau est lisse, moi-même, je me plais à la caresser. Femme libre, je suis, donc ce corps mousseux ne fera plus jamais l'objet de violence. C'est fini ! La déesse de la vie m'a redonné un souffle de vie. Alors, je respire à compte de mon corps. Je hurle j'ai faim de la liberté et de la douceur. J'ai grandi, pas besoin d'attendre l'approbation de quelqu'un. Je sais. En me contemplant, je me découvre tel un enfant en âge de parler. Il crie, soliloque, chante, reprend les noms de sa famille, surtout celui de son papa. Sauf que je ne suis pas enfant de chant, mais celui qui a grandi et s'est devenu maître, qui porte maintenant un regard différent sur ce monde. Il est son propre maître de philosophie. Alors, il rentre à l'intérieur de sa forteresse, il y trouve des réponses, et si le contraire se produit, il part chercher ailleurs, auprès des personnes aux intelligences figées, plutôt vers celles ayant mis leur cerveau à jour.",
           "isbn": "978-2-494686-02-1",
           "price": "12€",
-          "author_info": "Spécialiste des soins palliatifs, Moustapha Samb est un jeune Sénégalais, originaire de la région de Thiès, résident en Allemagne. Après avoir obtenu son Baccalauréat en 2016, Samb intègre l'université Cheikh Anta Diop de Dakar, faculté des Lettres et sciences humaines, département langues et civilisations germaniques. L'année suivante, il s'installe en Allemagne pour poursuivre son cursus."
+          "author_info": "Spécialiste des soins palliatifs, Moustapha Samb est un jeune Sénégalais, originaire de la région de Thiès, résident en Allemagne. Après avoir obtenu son Baccalauréat en 2016, Samb intègre l'université Cheikh Anta Diop de Dakar, faculté des Lettres et sciences humaines, département langues et civilisations germaniques. L'année suivante, il s'installe en Allemagne pour poursuivre son cursus.",
+          "image": "/dans-la-peau-dune-femme.jpg"
      },
      {
           "title": "Afrisiti",
@@ -37,7 +39,8 @@ const books = [
           "description": "Cette œuvre a pour thème principal la superstition en Afrique subsaharienne. À travers huit nouvelles qui se situent dans le triptyque temporel, Ben Amar Faye tente de montrer comment la sorcellerie a mené vers l'esclavage et le sous-développement, comment la superstition contribue à la fuite des cerveaux, comment elle conduit à l'infanticide, au travail des enfants et à d'autres problèmes. En résumé, l'œuvre cherche à représenter comment la superstition peut être la source des problèmes qui gangrènent l'Afrique.",
           "isbn": "978-2-494686-01-4",
           "price": "10€",
-          "author_info": "Ben Amar Faye est un germaniste sénégalais originaire de Thiès qui vit en Allemagne et est le fondateur des Éditions Ceddo. Il est l'auteur du recueil de nouvelles intitulé 'Carton rouge' (Le Jour) qui parle des réalités des émigrés en terre allemande. Ses textes sont des récits de mélanges de mysticisme, de géopolitique et d'hybridité culturelle, qui met l'accent sur les problèmes de l'Afrique."
+          "author_info": "Ben Amar Faye est un germaniste sénégalais originaire de Thiès qui vit en Allemagne et est le fondateur des Éditions Ceddo. Il est l'auteur du recueil de nouvelles intitulé 'Carton rouge' (Le Jour) qui parle des réalités des émigrés en terre allemande. Ses textes sont des récits de mélanges de mysticisme, de géopolitique et d'hybridité culturelle, qui met l'accent sur les problèmes de l'Afrique.",
+          "image": "/afrisiti.jpg"
      },
      {
           "title": "Trop de nationalisme tue le patriotisme",
@@ -46,7 +49,8 @@ const books = [
           "description": "Mamadou, alias Ndéréng, un enfant né, grandi et éduqué sur la terre sénégalaise. Un jeune homme qui n'avait jamais douté de sa sénégalité. Mais, hélas, c'est au moment de faire les démarches pour obtenir sa carte d'identité nationale afin de passer le baccalauréat qu'il a su que la loi ne le considérait pas comme sénégalais. Un choc qui vint bouleverser tout son existence. Certes, ce choc psychologique de besoin d'appartenance a farouchement secoué son patriotisme, mais sa détermination et son esprit révolutionnaire l'ont poussé à faire face à cette situation afin de ne laisser personne bafouer sa nature propre, mais aussi celle de plusieurs enfants sénégalais qui sont victimes de discrimination à cause de l'hybridité. Puisque cette situation a entravé ses études, il prit son courage à deux mains avec ses collaborateurs pour entreprendre plusieurs démarches différentes, en commençant par mettre sur pied un mouvement patriotique et citoyen dans le but de raffermir l'adoration sénégalaise de leur sol, mais aussi de contribuer à renforcer la cohésion sociale dans le respect et l'égalité de tous les Sénégalais sans discrimination d'origine. Après un long moment de combat sans résultats notoires, le jeune homme décide enfin de passer à l'étape supérieure en commettant l'innommable.",
           "isbn": "978-2-494686-06-9",
           "price": "12€",
-          "author_info": "Mohamadou Madina DIALLO (MMD) est de Soloum - comme sa mère - et a grandi à Rufisque. Après l'obtention de son Certificat d'aptitude à l'Enseignement secondaire (CAES) à la Faculté des Sciences et Technologies de l'Éducation et de la Formation, il a poursuivi ses études de géographie à l'université Cheikh Anta Diop de Dakar où il a également obtenu son diplôme de Master en Art, Culture et Société. Il a aussi été président de l'association 'Les Amis du Livre de Thies', membre de l'association des Arts, Cultures et Sports d'Afrique (ARCV) et il est actuellement enseignant-chercheur à l'université de Thiès, après avoir obtenu son doctorat de 3e cycle (PhD)."
+          "author_info": "Mohamadou Madina DIALLO (MMD) est de Soloum - comme sa mère - et a grandi à Rufisque. Après l'obtention de son Certificat d'aptitude à l'Enseignement secondaire (CAES) à la Faculté des Sciences et Technologies de l'Éducation et de la Formation, il a poursuivi ses études de géographie à l'université Cheikh Anta Diop de Dakar où il a également obtenu son diplôme de Master en Art, Culture et Société. Il a aussi été président de l'association 'Les Amis du Livre de Thies', membre de l'association des Arts, Cultures et Sports d'Afrique (ARCV) et il est actuellement enseignant-chercheur à l'université de Thiès, après avoir obtenu son doctorat de 3e cycle (PhD).",
+          "image": "/trop-de-nationalisme.jpg"
      },
      {
           "title": "À bout de souffle",
@@ -55,7 +59,8 @@ const books = [
           "description": "Le soleil scintille aux premières lueurs du matin. Il est huit heures. J'ouvre l'œil droit ; je suis content d'avoir récupéré de la fatigue de la veille. L'estomac creux, je m'empresse d'ingurgiter du jus de citron ; histoire de me désintoxiquer. Une habitude, dirais-je ! Mon esprit n'a pas pu s'éveiller. Encore une journée de labeur m'attend. Je me réjouis qu'il en soit ainsi. C'est mon médicament, ma dose d'adrénaline. Vous allez comprendre la raison. Un esprit libre des plus ordinaires, mais des plus courageux et trevuées. Je trotte de l'être presseur au sujet. De l'important et comique en atteignant la cible, à toi, sans pudeur ni préjugés. Cet écrit vient du cœur, de pensées labyrinthiques. Des sillons veineux, au sang du dré mon corps en déflagation ; tout y est. Je charrie la muse comme ultime arme de ma longue complainte silencieuse, de l'agonie invincible et indicible qui m'attend. Où vais-je atterrir ? Où que je me voie ? Suis-je perdu, coincé dans le néant ? Je me cherche dans...",
           "isbn": "978-2-494686-08-3",
           "price": "12€",
-          "author_info": "Né à Tambacounda (Sénégal), région orientale du Sénégal, Younouss NDIAYE est titulaire d'un certificat d'aptitude à l'enseignement secondaire (CAES) à la faculté des sciences et technologies de l'éducation et de la formation de Dakar, et d'un doctorat de 3e cycle en Littératures francophones africaines. Il est enseignant et chercheur à l'université Cheikh Anta Diop de Dakar. Il a publié de nombreuses recherches dans le domaine des littératures francophones, de la traduction et de la sociologie de la lecture. En 2023, il publie également ses premières œuvres littéraires. Lauréat de l'Abdoul Mamadou Macky SECK (Thiès - Sénégal) de reconnaissance pour son engagement dans le domaine littéraire."
+          "author_info": "Né à Tambacounda (Sénégal), région orientale du Sénégal, Younouss NDIAYE est titulaire d'un certificat d'aptitude à l'enseignement secondaire (CAES) à la faculté des sciences et technologies de l'éducation et de la formation de Dakar, et d'un doctorat de 3e cycle en Littératures francophones africaines. Il est enseignant et chercheur à l'université Cheikh Anta Diop de Dakar. Il a publié de nombreuses recherches dans le domaine des littératures francophones, de la traduction et de la sociologie de la lecture. En 2023, il publie également ses premières œuvres littéraires. Lauréat de l'Abdoul Mamadou Macky SECK (Thiès - Sénégal) de reconnaissance pour son engagement dans le domaine littéraire.",
+          "image": "/a-bout-de-souffle.jpg"
      },
      {
           "title": "Marie Ange, l'aventurière",
@@ -64,7 +69,8 @@ const books = [
           "description": "Dans ce roman, l'auteur se plaît à raconter l'itinéraire inachevé d'une jeune fille qui s'appelle Marie Ange. Devenue orpheline très tôt à cause de la mort de son père, affrontant un environnement social hostile en absence permanente et incompréhensible de sa propre mère, Ange s'est livrée elle-même, elle doit confronter, seule, d'atroces réalités sociales.",
           "isbn": "978-2-494686-02-1",
           "price": "12€",
-          "author_info": "Mame Omar KEBE est né dans la région de Kaolack à Keur Madiabel. C'est là où il a fait toutes ses études jusqu'à l'obtention du Bac. Ensuite, il s'inscrit à l'université Cheikh Anta Diop de Dakar. Diplômé en sciences humaines et sociales, il a participé à plusieurs recherches universitaires. Ainsi, il continue de donner des cours dans le domaine de la philosophie et de la sociologie. En 2023, il publie également son premier roman 'Marie Ange, l'aventurière' qui raconte l'histoire d'une jeune femme qui doit faire face à des défis sociaux et personnels tout en cherchant à réussir dans la vie. Ce roman est une exploration de la résilience et de la force de caractère face aux adversités."
+          "author_info": "Mame Omar KEBE est né dans la région de Kaolack à Keur Madiabel. C'est là où il a fait toutes ses études jusqu'à l'obtention du Bac. Ensuite, il s'inscrit à l'université Cheikh Anta Diop de Dakar. Diplômé en sciences humaines et sociales, il a participé à plusieurs recherches universitaires. Ainsi, il continue de donner des cours dans le domaine de la philosophie et de la sociologie. En 2023, il publie également son premier roman 'Marie Ange, l'aventurière' qui raconte l'histoire d'une jeune femme qui doit faire face à des défis sociaux et personnels tout en cherchant à réussir dans la vie. Ce roman est une exploration de la résilience et de la force de caractère face aux adversités.",
+          "image": "/marie-ange-laventuriere.jpg"
      },
      {
           "title": "Un Français sans papiers",
@@ -73,7 +79,18 @@ const books = [
           "description": "Depuis avant, il m'arrive de perdre ton visage. Ce visage que j'ai vu pendant des jours lorsque nous étions enfermés dans une maison en Libye. Aujourd'hui, je cherche autour de moi ton visage qui a disparu. Malgré tout, je reste figé et abasourdi. Je suis de plus en plus écrasé par un chagrin et rendu plus déchirant encore par les centaines de personnes qui meurent dans la Méditerranée, dans le désert et dans les frontières. Parfois, dans mon sommeil, j'entends encore le cri des gens qui se sont noyés. Leurs visages existent autant que leurs voix. Ils reviennent en force et en éclat souvent. Ils m'appellent comme s'ils voulaient me confier quelque chose que j'ignore. Ainsi, j'ai réagi en brisant mes mots, souvent sur des notes tristes comme un combat existentiel, que je mène pour dénoncer les traitements inhumains et les violences exercées sur nous. De vos morts et de vos souffrances encore, j'emprunte de la colère et de la douleur pour écrire ce texte, pour ne pas taire l'essentiel. Notre vie est désertée, comme s'il n'y avait jamais de demain. C'est ce qui nous a permis de grandir à la dure échelle d'une réalité forte et sans espoir. Les jours de nos morts et de nos migrants, les autres sont des blancs et des réfugiés ; cela, je ne pourrais jamais l'oublier. Et la forme la plus triste de cette réalité, est que notre existence à nous noirs n'est négative. Je veux dire, notre histoire, parce que nos identités ne sont pas négociables et nos trajectoires sont politiques.",
           "isbn": "978-2-494686-09-0",
           "price": "13€",
-          "author_info": "Kab Niang est né en 1988 issu d'un quartier populaire de la banlieue de Dakar. Il vit et travaille à Paris. Diplômé d'un master 2 en littérature francophone à l'université de Saint-Denis Paris 8, Kab est également enseignant. Écrivain engagé, il participe activement aux débats sociopolitiques sur les questions migratoires et les droits des personnes réfugiées. Ses œuvres littéraires sont souvent inspirées de sa propre expérience et de celles des migrants et réfugiés qu'il côtoie. 'Un Français sans papiers' est son premier roman où il décrit avec émotion et réalisme les défis auxquels font face les personnes sans papiers."
+          "author_info": "Kab Niang est né en 1988 issu d'un quartier populaire de la banlieue de Dakar. Il vit et travaille à Paris. Diplômé d'un master 2 en littérature francophone à l'université de Saint-Denis Paris 8, Kab est également enseignant. Écrivain engagé, il participe activement aux débats sociopolitiques sur les questions migratoires et les droits des personnes réfugiées. Ses œuvres littéraires sont souvent inspirées de sa propre expérience et de celles des migrants et réfugiés qu'il côtoie. 'Un Français sans papiers' est son premier roman où il décrit avec émotion et réalisme les défis auxquels font face les personnes sans papiers.",
+          "image": "/un-francais-sans-papiers.jpg"
+     },
+     {
+          "title": "Dictature et Dictateurs en Afrique",
+          "author": "Thomas-Emmanuel Mumba",
+          "genre": "Essai",
+          "description": "Ce livre explore l'histoire des dictatures en Afrique, mettant en lumière les pratiques autoritaires et les régimes oppressifs qui ont marqué le continent. À travers une analyse détaillée, l'auteur examine comment les dictateurs africains ont consolidé leur pouvoir et l'impact de leurs régimes sur les populations. Le livre offre également une réflexion sur les résistances et les luttes pour la démocratie en Afrique.",
+          "isbn": "978-2-494686-10-6",
+          "price": "15€",
+          "author_info": "Thomas-Emmanuel Mumba est un historien et politologue africain. Il a consacré sa carrière à l'étude des régimes politiques en Afrique et a publié plusieurs ouvrages sur les dynamiques de pouvoir sur le continent. 'Dictature et Dictateurs en Afrique' est son dernier essai, dans lequel il offre une perspective critique et informée sur les systèmes autoritaires en Afrique.",
+          "image": "/dictature-et-dictateurs.jpg"
      }
 ];
 
@@ -96,7 +113,8 @@ export function CatalogueSection() {
                                              <CardDescription>{book.author}</CardDescription>
                                         </CardHeader>
                                         <CardContent>
-                                             <p className="text-muted-foreground">
+                                             <Image src={book.image} alt={book.title} width={300} height={400} className="object-cover" />
+                                             <p className="mt-2 text-muted-foreground">
                                                   {book.description.slice(0, 105)}...
                                              </p>
                                         </CardContent>

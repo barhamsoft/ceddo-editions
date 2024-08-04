93e568b813fc339fa4988fe5b978858e41668206
bakseck <seck.bakar@ugb.edu.sn>
2024-08-04 01:46:04 +0000

    templates

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export default function Home() {
      return (
-          <div>
-               <Link href="/1">
-                    <a>Link to /1</a>
-               </Link>
-               <br />
-               <Link href="/2">
-                    <a>Link to /2</a>
-               </Link>
-               <br />
-               <Link href="/3">
-                    <a>Link to /3</a>
-               </Link>
+          <div className="flex items-center justify-center h-min-screen">
+               <div className="flex flex-col items-center space-y-4">
+                    <Link href="/1" legacyBehavior>
+                         <a className="px-4 py-2 text-white bg-blue-500 rounded-md">/1</a>
+                    </Link>
+                    <Link href="/2" legacyBehavior>
+                         <a className="px-4 py-2 text-white bg-blue-500 rounded-md">/2</a>
+                    </Link>
+                    <Link href="/3" legacyBehavior>
+                         <a className="px-4 py-2 text-white bg-blue-500 rounded-md">/3</a>
+                    </Link>
+               </div>
           </div>
      );
 };
